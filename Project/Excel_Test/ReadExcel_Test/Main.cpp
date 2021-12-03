17838dde78c9b2803b4cad85919c4f4c859d81ce
Duyong92 <sigler92@naver.com>
2021-12-03 17:11:38 +0900

    211203 1711 first attempt

--- Project/Excel_Test/ReadExcel_Test/Main.cpp
+++ Project/Excel_Test/ReadExcel_Test/Main.cpp
@@ -8,8 +8,36 @@
 
 int main() 
 {
-	std::vector<std::vector<char*>> Excel;
-	Excel = ReadExcel_Test();
-	std::cout << Excel[3][0] << std::endl;
+	std::cout << "임의로 '../read_csv_test.csv' 경로에 있는 csv를 읽겠습니다." << std::endl;
+	Excel_CSV Excel;
+	std::vector<std::vector<char*>> csv = Excel.GetCSV();
+	// 시간대 계산을 위해 필요한 임시 변수들이다.
+	unsigned int second;
+	unsigned int minute;
+	unsigned int hour;
+
+	std::cout << "성공적으로 csv를 읽었습니다." << std::endl << std::endl;
+	std::cout << "필요한 데이터의 시간대를 시/분/초 순서대로 입력해주세요." << std::endl;
+	std::cin >> hour;
+	std::cin >> minute;
+	std::cin >> second;
+	second += (minute * 60 + hour * 3600);
+	for(int i = 1; i < Excel.GetMaxColumn(); i++)
+	{
+		std::cout << csv[0][i] << " ";
+	}
+	std::cout << std::endl;
+	for (int j = 2; j < Excel.GetMaxRow(); j++)
+	{
+		if (std::stoi(csv[j][0]) == second)
+		{
+			for (int i = 1; i < Excel.GetMaxColumn(); i++)
+			{
+				std::cout << csv[j][i] << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
 	return 0;
 }
--- Project/Excel_Test/ReadExcel_Test/ReadExcel_Test.h
+++ Project/Excel_Test/ReadExcel_Test/ReadExcel_Test.h
@@ -1,6 +1,23 @@
 #pragma once
 
-// 문자열을 문자 배열로 바꿔주는 함수이다.
+class Excel_CSV
+{
+private:
+	// 2차원 벡터의 열 크기이다.
+	unsigned int _unMaxColumn;
+	// 2차원 벡터의 행 크기이다.
+	unsigned int _unMaxRow;
+	// 총 센서의 수
+	unsigned int _unSensors;
+	// 문자 배열 벡터를 받을 2 차원 벡터이다.
+	std::vector<std::vector<char*>> vvpszData;
+public:
+	Excel_CSV();
+	std::vector<std::vector<char*>> GetCSV();
+	unsigned int GetMaxRow();
+	unsigned int GetMaxColumn();
+};
+
 char* to_CharPoint(std::string _sBuffer)
 {
 	// 셀을 받을 문자 배열이다.
@@ -18,7 +35,7 @@ char* to_CharPoint(std::string _sBuffer)
 	return pszCell;
 }
 
-// HH:MM:ss 시간 표기 문자열인지 확인하고 초 단위 문자열로 바꿔주는 함수이다.
+
 std::string DateFormat_Second(std::string _sBuffer)
 {
 	std::string temp_sBuffer = _sBuffer;
@@ -51,7 +68,7 @@ std::string DateFormat_Second(std::string _sBuffer)
 }
 
 // fopen 의 인자로 char* 을 입력하게 매개변수를 받을 것이다.
-std::vector<std::vector<char*>> ReadExcel_Test()
+Excel_CSV::Excel_CSV()
 {
 	// fopen 의 인자로 char* 의 filename 을 입력 받게 할 것이다.
 	// 읽기 전용 테스트모드로 파일을 연다.
@@ -63,11 +80,12 @@ std::vector<std::vector<char*>> ReadExcel_Test()
 	// 임시로 문자열을 보관할 변수이다.
 	std::string _sBuffer = "";
 
-	// 문자 배열 벡터를 받을 2 차원 벡터이다.
-	std::vector<std::vector<char*>> vvpszData;
-
 	// 문자 배열을 받을 벡터이다.
 	std::vector<char*> vpszData;
+
+	// 최대 행렬을 초기화 해준다.
+	_unMaxColumn = 0;
+	_unMaxRow = 0;
 
 	// 파일이 존재하지 않을 경우 전처리를 한다.
 	if (fp == NULL)
@@ -91,11 +109,18 @@ std::vector<std::vector<char*>> ReadExcel_Test()
 				// 문자 배열 벡터를 2 차원 벡터에 넣는다.
 				vvpszData.push_back(vpszData);
 
+				// 최대 행 크기를 갱신다.
+				if (_unMaxColumn < vpszData.size())
+					_unMaxColumn = vpszData.size();
+
 				// 문자 배열 벡터를 초기화한다.
 				vpszData.clear();
 
 				// 임시 문자열을 초기화한다.
 				_sBuffer = "";
+
+				// 최대 열 크기를 갱신한다.
+				_unMaxRow++;
 			}
 			// 쉼표(아스키코드 44)일 경우
 			else if (_szASCII == 44)
@@ -114,5 +139,8 @@ std::vector<std::vector<char*>> ReadExcel_Test()
 		// 파일 닫기 실패 시 EOF 가 반환된다.
 		fclose(fp);
 	}
-	return vvpszData;
-}
+};
+
+std::vector<std::vector<char*>> Excel_CSV::GetCSV() { return vvpszData; };
+unsigned int Excel_CSV::GetMaxRow() { return _unMaxRow; };
+unsigned int Excel_CSV::GetMaxColumn() { return _unMaxColumn; };
