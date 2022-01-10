2130ae8cc351b526f70f2170d6f238c2415aa622
Duyong92 <sigler92@naver.com>
2022-01-10 17:56:07 +0900

    220110 1755 sort out window handler in cpbzLPCWSTR() later

--- Project/Utilities/Utilities/Convert.cpp
+++ Project/Utilities/Utilities/Convert.cpp
@@ -1,7 +1,6 @@
 #include "Convert.h"
 
-wchar_t* pbzPSZ(const char* _pbz)
-{
+wchar_t* pbzPSZ(const char* _pbz){
 	const size_t csi = strlen(_pbz) + 1;
 	wchar_t* psz = new wchar_t[csi];
 	
@@ -10,21 +9,25 @@ wchar_t* pbzPSZ(const char* _pbz)
 	return psz;
 }
 
-LPCWSTR pbzLPCWSTR(char* _pbz)
-{
-	return pbzLPCWSTR((const char*)_pbz);
+LPCWSTR strLPCWSTR(string _str) {
+	wchar_t* buf = new wchar_t[_str.length() + 1];
+	copy(_str.begin(), _str.end(), buf);
+	buf[_str.length()] = 0;
+
+	return buf;
 }
 
-LPCWSTR pbzLPCWSTR(string _str)
-{
-	return pbzLPCWSTR(_str.c_str());
-}
+LPCWSTR cpbzLPCWSTR(const char* _cpbz, UINT CodePage) {
+	wchar_t* buf = new wchar_t[strlen(_cpbz) + 1];
 
-LPCWSTR pbzLPCWSTR(const char* _cpbz)
-{
-	wchar_t* psz = new wchar_t[sizeof(_cpbz)*2];
-	// default ANSI CodePage
-	MultiByteToWideChar(CP_ACP, 0, _cpbz, -1, psz, sizeof(_cpbz) * 2);
+	if (MultiByteToWideChar(CodePage, 0, _cpbz, -1, buf, strlen(_cpbz) + 1) == 0) {
+		// 추후 Window handler로 따로 뺄 예정입니다.
+		TCHAR* tmp = 0;
+		FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+			0, GetLastError(), 0, (TCHAR*)&tmp, 0, 0);
 
-	return psz;
+		return buf;
+	}
+
+	return buf;
 }
